d808a411b526d66faa6aa08af86a5f18ff3a813c
Benjamin Stepp <me@benstepp.com>
2020-11-14 17:10:52 -0600

    keygen docs

--- ruby/lib/elixir_compat/plug_crypto.rb
+++ ruby/lib/elixir_compat/plug_crypto.rb
@@ -3,9 +3,20 @@ require 'elixir_compat/plug_crypto/key_generator'
 require 'elixir_compat/plug_crypto/message_verifier'
 
 module ElixirCompat
+  ##
+  # Ruby module that is mostly compatible with `Plug.Crypto` in elixir
+  #
+  # ##### Plug.Crypto External Links
+  # * [hex.pm](https://hex.pm/packages/plug_crypto)
+  # * [hexdocs](https://hexdocs.pm/plug_crypto/Plug.Crypto.html)
+  # * [github](https://github.com/elixir-plug/plug_crypto)
+  #
   module PlugCrypto
     class << self
 
+      ##
+      # Compares the two binaries in constant-time to avoid timing attacks.
+      #
       def secure_compare(a, b)
         OpenSSL.secure_compare(a, b)
       end
--- ruby/lib/elixir_compat/plug_crypto/key_generator.rb
+++ ruby/lib/elixir_compat/plug_crypto/key_generator.rb
@@ -2,6 +2,20 @@ require 'openssl'
 
 module ElixirCompat
   module PlugCrypto
+
+    ##
+    # KeyGenerator implements PBKDF2 (Password-Based Key Derivation Function
+    # 2), part of PKCS #5 v2.0 (Password-Based Cryptography Specification).
+    #
+    # It can be used to derive a number of keys for various purposes from a
+    # given secret. This lets applications have a single secure secret, but
+    # avoid reusing that key in multiple incompatible contexts.
+    #
+    # The returned key is a binary. You may invoke functions in the `Base64`
+    # module, such as `Base64.urlsafe_encode64`, to convert this binary into a
+    # textual representation.
+    #
+    # see http://tools.ietf.org/html/rfc2898#section-5.2
     module KeyGenerator
       class << self
 
@@ -13,6 +27,43 @@ module ElixirCompat
 
         MAX_LENGTH = (1 << 32) - 1
 
+        ##
+        # Generates a derived key suitable for use.
+        #
+        # ### Parameters
+        #
+        # * `secret` (String) - The single secret used. This is typically
+        # generated with `mix phx.gen.secret` in your elixir application
+        # * `salt` (String) - Salt used to generate a key.
+        # * `options` (Hash) - A hash of options used to generate the key.
+        #
+        # #### Options
+        #
+        # * `iterations` (Integer) - Default of 1000 (increase to at least 2^16 for passwords)
+        # * `length` (Integer) - A length in octets for the derived key (defaults to 32)
+        # * `digest` (Atom) - The digest type to be used for the pseudo random
+        # function. Allowed values include: `:sha`, `:sha1`, `:sha224`,
+        # `:sha256`, `:sha384`, or `:sha512`.
+        #
+        # ### Examples
+        #
+        # #### Generating a Key with salt
+        #
+        # ```elixir
+        # # elixir
+        #
+        # secret = Application.get_env(:my_app, :secret_key_base)
+        # Plug.Crypto.KeyGenerator.generate(secret, "salt")
+        # ```
+        #
+        # ```ruby
+        # # ruby
+        #
+        # secret = Rails.application.secrets.secret_key_base
+        # ElixirCompat::PlugCrypto::KeyGenerator.generate(secret, "salt")
+        # ```
+        #
+        #
         def generate(secret, salt, options = {})
           options = DEFAULT_OPTIONS.merge(options)
           check_iterations(options)
